6ba1b5d61602d834b22dd53410b033c4c209ed39
Piotr Grzeszczak <piotrek32@gmail.com>
2016-05-23 00:13:55 +0200

    iflist.py: Naming fixes

--- iflist.py
+++ iflist.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# iflist.py - Utility to lis all network interfaces.
+# iflist.py - Utility to list all network interfaces.
 # Version: 0.1
 # (c) 2016 Piotr Grzeszczak
 # http://www.grzeszczak.pw
@@ -11,8 +11,9 @@ import netifaces
 for inet_face in netifaces.interfaces():
     print("Interface: {}".format(inet_face))
     ifaddr = netifaces.ifaddresses(inet_face)
-    for a in ifaddr:
-        print("  {}:".format(a))
-        for b in ifaddr[a]:
-            for c in b:
-                print("    {}: {}".format(c, b[c]))
+    for addr_id in ifaddr:
+        print("  {}:".format(addr_id))
+        for addr_num in ifaddr[addr_id]:
+            for addr_val in addr_num:
+                print("    {}: {}".format(addr_val, addr_num[addr_val]))
+    print("")
